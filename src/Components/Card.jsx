897eeac33f0d072253d7824b04a4279c6d7d2eb1
ArshikAk <arshik0404@gmail.com>
2024-02-29 16:08:47 +0530

    final commit

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,10 +4,8 @@ import "../Styles/style.css"
 
 function Card({logo,content}) {
   return (
-    // eslint-disable-next-line react/jsx-no-comment-textnodes
     <div className='List'>
-      // eslint-disable-next-line, jsx-a11y/img-redundant-alt
-      <img src={logo} alt="Image-Not_Found" className='content-image'/>
+      <img src={logo} alt="Image-Not-Found" className='content-image'/>
 
       <div className='right'>
         <h2>{content}</h2>
