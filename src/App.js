d728a00746f2ef735b393123ec8f2c1f4874e966
ArshikAk <arshik0404@gmail.com>
2024-03-27 09:34:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
 import "./Styles/style.css"
 import ReactDOM from "react-dom/client";
-import Home from "./Pages/home.js"
+import Home from './Pages/home';
 
 function App() {
   return (
     <div className="App">
-       {/* <BrowserRouter>
-      <Routes>
-         <Route element={<Home/>}></Route>
-        <Route path='/Card' element={<Card/>}></Route>
-
-      </Routes>
-      </BrowserRouter>  */}
       <Home></Home>
     </div>
   );
